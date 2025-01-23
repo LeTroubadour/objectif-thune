7870662da3602ce73401930907e38f316f8a3ad8
LeTroubadour <arnaud.prigent1@gmail.com>
2025-01-23 22:27:01 +0100

    Liste des ROIs et Probabilités pour les Actifs Alternatifs

---
+++ actifsAlternatifs_rates.js
@@ -0,0 +1,19 @@
+/*
+// Liste des ROIs et Probabilités pour les Actifs Alternatifs
+*/
+
+export const actifsAlternatifsROIs = {
+    "3ans": [
+      { roiFinal: 25, probability: 10 },
+      { roiFinal: 10, probability: 40 },
+      { roiFinal: 0, probability: 20 },
+      { roiFinal: -10, probability: 30 },
+    ],
+    "7ans": [
+      { roiFinal: 50, probability: 10 },
+      { roiFinal: 20, probability: 40 },
+      { roiFinal: 5, probability: 30 },
+      { roiFinal: -15, probability: 20 },
+    ],
+  };
+  
